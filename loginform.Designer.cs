4d2633c495a386965e437f0a83b5cfa50000975a
diduli5791 <diduliwijesiri@gmail.com>
2025-05-14 11:46:13 +0530

    Add files via upload

---
+++ loginform.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Event_Ticketing_System
+{
+    partial class loginform
+    {
+        private System.ComponentModel.IContainer components = null;
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.login_label = new System.Windows.Forms.Label();
+            this.login_showPass = new System.Windows.Forms.CheckBox();
+            this.login_password = new System.Windows.Forms.TextBox();
+            this.login_username = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.pasword = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightYellow;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.login_label);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 450);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // login_label
+            // 
+            this.login_label.AutoSize = true;
+            this.login_label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.login_label.Location = new System.Drawing.Point(35, 208);
+            this.login_label.Name = "login_label";
+            this.login_label.Size = new System.Drawing.Size(225, 22);
+            this.login_label.TabIndex = 16;
+            this.login_label.Text = "Event Ticketing System";
+            // 
+            // login_showPass
+            // 
+            this.login_showPass.AutoSize = true;
+            this.login_showPass.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_showPass.Location = new System.Drawing.Point(601, 290);
+            this.login_showPass.Margin = new System.Windows.Forms.Padding(4);
+            this.login_showPass.Name = "login_showPass";
+            this.login_showPass.Size = new System.Drawing.Size(112, 18);
+            this.login_showPass.TabIndex = 22;
+            this.login_showPass.Text = "Show Password";
+            this.login_showPass.UseVisualStyleBackColor = true;
+            this.login_showPass.CheckedChanged += new System.EventHandler(this.login_showPass_CheckedChanged);
+            // 
+            // login_password
+            // 
+            this.login_password.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_password.Location = new System.Drawing.Point(355, 246);
+            this.login_password.Margin = new System.Windows.Forms.Padding(4);
+            this.login_password.Multiline = true;
+            this.login_password.Name = "login_password";
+            this.login_password.PasswordChar = '*';
+            this.login_password.Size = new System.Drawing.Size(376, 36);
+            this.login_password.TabIndex = 21;
+            // 
+            // login_username
+            // 
+            this.login_username.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_username.Location = new System.Drawing.Point(355, 158);
+            this.login_username.Margin = new System.Windows.Forms.Padding(4);
+            this.login_username.Multiline = true;
+            this.login_username.Name = "login_username";
+            this.login_username.Size = new System.Drawing.Size(376, 36);
+            this.login_username.TabIndex = 20;
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.LightYellow;
+            this.login_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_btn.FlatAppearance.BorderSize = 0;
+            this.login_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumTurquoise;
+            this.login_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumTurquoise;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_btn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.Color.Black;
+            this.login_btn.Location = new System.Drawing.Point(355, 335);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(90, 35);
+            this.login_btn.TabIndex = 19;
+            this.login_btn.Text = "LOGIN";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // pasword
+            // 
+            this.pasword.AutoSize = true;
+            this.pasword.Location = new System.Drawing.Point(351, 220);
+            this.pasword.Name = "pasword";
+            this.pasword.Size = new System.Drawing.Size(56, 13);
+            this.pasword.TabIndex = 18;
+            this.pasword.Text = "Password:";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(351, 132);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(58, 13);
+            this.username.TabIndex = 17;
+            this.username.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(350, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Login";
+            // 
+            // loginform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.login_showPass);
+            this.Controls.Add(this.login_password);
+            this.Controls.Add(this.login_username);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.pasword);
+            this.Controls.Add(this.username);
+            this.Name = "loginform";
+            this.Text = "Login_Form";
+            this.Load += new System.EventHandler(this.loginform_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox login_showPass;
+        private System.Windows.Forms.TextBox login_password;
+        private System.Windows.Forms.TextBox login_username;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Label pasword;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label login_label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ loginform.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Event_Ticketing_System
+{
+    public partial class loginform : Form
+    {
+        public string UserRole { get; private set; }
+        public loginform()
+        {
+            InitializeComponent();
+        }
+
+        private void loginform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_showPass_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            DB_MY db = new DB_MY();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+
+     
+            MySqlCommand command = new MySqlCommand("SELECT `role` FROM `users` WHERE `username` = @username AND `password` = @password", db.GetConnection());
+
+        
+            command.Parameters.Add("@username", MySqlDbType.VarChar).Value = login_username.Text;
+            command.Parameters.Add("@password", MySqlDbType.VarChar).Value = login_password.Text;
+
+            try
+            {
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                if (table.Rows.Count > 0)
+                {
+                
+                    UserRole = table.Rows[0]["role"].ToString().ToLower();
+
+                    MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password!", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
